f4e1d3ed060bd60ea67d6b7669f2451a2800a41f
Elton Fonseca <elton869@gmail.com>
2021-09-25 16:03:45 -0300

    02 - Criando o projeto e o controller

---
+++ app/Http/Controllers/CarrinhoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarrinhoController extends Controller
+{
+    public function listar()
+    {
+        dd('listar');
+    }
+
+    public function adicionar()
+    {
+        dd('adicionar');
+    }
+}
